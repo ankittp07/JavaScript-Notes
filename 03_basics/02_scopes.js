686884d6d87debd9b77e9c1df9c98fb66fa88935
Ankit <ankitpoonia9721@gmail.com>
2024-08-25 14:55:49 +0000

    scopes and mini hoisting in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,73 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+// console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+// console.log(myName)
+// let myName = 'ankit'
+
+
+
+
+
+/* 
+to learn about scope and mini hoisting properly--
+part 1 :
+https://youtu.be/cHHU0jXfjKY?feature=shared
+part 2 :
+https://youtu.be/eWwge2YpHhc?feature=shared
+*/
