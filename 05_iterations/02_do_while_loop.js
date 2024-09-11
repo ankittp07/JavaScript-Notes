abb016334cbef5b967d3b0a9a902b25016305f60
Ankit <ankitpoonia9721@gmail.com>
2024-09-11 13:03:29 +0000

    while and do-while loops

---
+++ 05_iterations/02_do_while_loop.js
@@ -0,0 +1,41 @@
+
+// while loop
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// do while loop
+
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+// condition check done after printing the value, so....
+let score2 = 11
+
+do {
+    console.log(`Score is ${score2}`);
+    score++
+} while (score <= 10);
